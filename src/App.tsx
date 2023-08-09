f8678143a6355f9a3b6fc9014672e83ebdcc2066
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-09 14:35:53 -0400

    Create the Shuffle Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './Components/Header';
+import { cardImages } from './Components/Images';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
-    Hello
+      <Header turns={turns} onShuffle={shuffleCards}/>
     </div>
   );
 }
