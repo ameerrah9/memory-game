1d3b008e0a8a68b1909af20eeeec3798a96918c9
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-11 10:14:48 -0400

    Develop the Grid Application

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import Card from './Card';
+
+const Grid = ({ cards, choiceOne, choiceTwo, disabled, handleChoice }) => {
+  return (
+    <div className="card-grid">
+      {cards.map((card) => (
+        <div className="card" key={card.id}>
+          <Card
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        </div>
+      ))}
+    </div>
+  )
+}
+export default Grid;
