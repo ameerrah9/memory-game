a889cc6b27437f4fbdf60415d360d8d342d2c642
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-08-11 10:07:32 -0400

    Develop the Grid Cell

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,1 +1,22 @@
 import React from 'react';
+import DefaultImage from '../Images/ermelinda-martin-Bu_9GlQe8uI-unsplash.jpg';
+
+const Card = ({ card, flipped, disabled, handleChoice }) => {
+
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card" key={card.id}>
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="card front" className="front" />
+        <img onClick={handleClick} src={DefaultImage} alt="card back" className="back" />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
